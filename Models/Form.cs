835ddd25310cb745bad32de218daf9a04cf27c8f
Cristina Martinez <mcristina.mavila@gmail.com>
2023-11-23 11:55:22 -0600

    Remove example code. Add required values

--- Models/Form.cs
+++ Models/Form.cs
@@ -2,8 +2,8 @@ namespace FormAPI.Models;
 
 public class Form {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
     
     public string? Secret { get; set; }
 }
